c3a4777a5f773c91a29480b2b7c41211b15294e6
Sofia Chatzopoulou <sofaki12@msn.com>
2017-03-28 01:03:19 +0300

    Ola ektos synplyn kai not second dot seems to be ok

--- app/src/main/java/com/example/sofak/fastcalculator/MainActivity.java
+++ app/src/main/java/com/example/sofak/fastcalculator/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.sofak.fastcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 //import android.widget.TextView;
@@ -13,8 +14,9 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    int count = 0, countsyn = 0;
+    int count = 0, countsyn = 0 , tvend;
     String old;
+    char aChar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
         clea.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View viewrt) {
+                aChar = tv.getText().charAt(0);
+                Log.d("Char at index 0",  Character.toString(aChar) );
                 tv.setText("0");
             }
         });
@@ -42,13 +46,25 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View viewrt) {
                 count++;
-                old = tv.getText().toString();
-                if (count%2==1) {
-
-                    tv.setText(old + "(");
-                }
-                else{
-                    tv.setText(old + ")");
+                aChar = tv.getText().charAt(0);
+                //tvend = tv.getText().length() - 1;
+                //old = tv.getText().toString();
+                if (aChar == '0' ){
+                    if (count%2==1) {
+                        tv.setText("(");
+                    }
+                    else{
+                        tv.setText(")");
+                    }
+                }else
+                {
+                    old = tv.getText().toString();
+                    if (count%2==1) {
+                        tv.setText(old + "(");
+                    }
+                    else{
+                        tv.setText(old + ")");
+                    }
                 }
             }
         });
@@ -89,89 +105,219 @@ public class MainActivity extends AppCompatActivity {
         oxtw.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View viewrt) {
-                old = tv.getText().toString();
-                tv.setText(old + "8");
+                aChar = tv.getText().charAt(0);
+                tvend = tv.getText().length() - 1;
+                if (aChar == '0' ){
+                    if (tv.getText().length()>1){
+                        old = tv.getText().subSequence(1,tvend).toString();
+                        tv.setText(old + "8");
+                    }else
+                    {
+                        tv.setText("8");
+                    }
+                }else
+                {
+                    old = tv.getText().toString();
+                    tv.setText(old + "8");
+                }
             }
         });
         Button ennia = (Button) findViewById(R.id.nine);
         ennia.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View viewrt) {
-                old = tv.getText().toString();
-                tv.setText(old + "9");
+                aChar = tv.getText().charAt(0);
+                tvend = tv.getText().length() - 1;
+                if (aChar == '0' ){
+                    if (tv.getText().length()>1){
+                        old = tv.getText().subSequence(1,tvend).toString();
+                        tv.setText(old + "9");
+                    }else
+                    {
+                        tv.setText("9");
+                    }
+                }else
+                {
+                    old = tv.getText().toString();
+                    tv.setText(old + "9");
+                }
             }
         });
         Button efta = (Button) findViewById(R.id.seven);
         efta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View viewrt) {
-                old = tv.getText().toString();
-                tv.setText(old + "7");
+                aChar = tv.getText().charAt(0);
+                tvend = tv.getText().length() - 1;
+                if (aChar == '0' ){
+                    if (tv.getText().length()>1){
+                        old = tv.getText().subSequence(1,tvend).toString();
+                        tv.setText(old + "7");
+                    }else
+                    {
+                        tv.setText("7");
+                    }
+                }else
+                {
+                    old = tv.getText().toString();
+                    tv.setText(old + "7");
+                }
             }
         });
         Button exi = (Button) findViewById(R.id.six);
         exi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View viewrt) {
-                old = tv.getText().toString();
-                tv.setText(old + "6");
+                aChar = tv.getText().charAt(0);
+                tvend = tv.getText().length() - 1;
+                if (aChar == '0' ){
+                    if (tv.getText().length()>1){
+                        old = tv.getText().subSequence(1,tvend).toString();
+                        tv.setText(old + "6");
+                    }else
+                    {
+                        tv.setText("6");
+                    }
+                }else
+                {
+                    old = tv.getText().toString();
+                    tv.setText(old + "6");
+                }
             }
         });
         Button pente = (Button) findViewById(R.id.five);
         pente.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View viewrt) {
-                old = tv.getText().toString();
-                tv.setText(old + "5");
+                aChar = tv.getText().charAt(0);
+                tvend = tv.getText().length() - 1;
+                if (aChar == '0' ){
+                    if (tv.getText().length()>1){
+                        old = tv.getText().subSequence(1,tvend).toString();
+                        tv.setText(old + "5");
+                    }else
+                    {
+                        tv.setText("5");
+                    }
+                }else
+                {
+                    old = tv.getText().toString();
+                    tv.setText(old + "5");
+                }
             }
         });
         Button tessera = (Button) findViewById(R.id.four);
         tessera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View viewrt) {
-                old = tv.getText().toString();
-                tv.setText(old + "4");
+                aChar = tv.getText().charAt(0);
+                tvend = tv.getText().length() - 1;
+                if (aChar == '0' ){
+                    if (tv.getText().length()>1){
+                        old = tv.getText().subSequence(1,tvend).toString();
+                        tv.setText(old + "4");
+                    }else
+                    {
+                        tv.setText("4");
+                    }
+                }else
+                {
+                    old = tv.getText().toString();
+                    tv.setText(old + "4");
+                }
             }
         });
         Button tria = (Button) findViewById(R.id.three);
         tria.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View viewrt) {
-                old = tv.getText().toString();
-                tv.setText(old + "3");
+                aChar = tv.getText().charAt(0);
+                tvend = tv.getText().length() - 1;
+                if (aChar == '0' ){
+                    if (tv.getText().length()>1){
+                        old = tv.getText().subSequence(1,tvend).toString();
+                        tv.setText(old + "3");
+                    }else
+                    {
+                        tv.setText("3");
+                    }
+                }else
+                {
+                    old = tv.getText().toString();
+                    tv.setText(old + "3");
+                }
             }
         });
         Button duo = (Button) findViewById(R.id.two);
         duo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View viewrt) {
-                old = tv.getText().toString();
-                tv.setText(old + "2");
+                aChar = tv.getText().charAt(0);
+                tvend = tv.getText().length() - 1;
+                if (aChar == '0' ){
+                    if (tv.getText().length()>1){
+                        old = tv.getText().subSequence(1,tvend).toString();
+                        tv.setText(old + "2");
+                    }else
+                    {
+                        tv.setText("2");
+                    }
+                }else
+                {
+                    old = tv.getText().toString();
+                    tv.setText(old + "2");
+                }
             }
         });
         Button ena = (Button) findViewById(R.id.one);
         ena.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View viewrt) {
-                old = tv.getText().toString();
-                tv.setText(old + "1");
+                aChar = tv.getText().charAt(0);
+                tvend = tv.getText().length() - 1;
+                if (aChar == '0' ){
+                    if (tv.getText().length()>1){
+                        old = tv.getText().subSequence(1,tvend).toString();
+                        tv.setText(old + "1");
+                    }else
+                    {
+                        tv.setText("1");
+                    }
+                }else
+                {
+                    old = tv.getText().toString();
+                    tv.setText(old + "1");
+                }
             }
         });
         Button mhden = (Button) findViewById(R.id.zero);
         mhden.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View viewrt) {
-                old = tv.getText().toString();
-                tv.setText(old + "0");
+                aChar = tv.getText().charAt(0);
+                tvend = tv.getText().length() - 1;
+                if (aChar == '0' ){
+                    if (tv.getText().length()>1){
+                        old = tv.getText().subSequence(1,tvend).toString();
+                        tv.setText(old + "0");
+                    }else
+                    {
+                        tv.setText("0");
+                    }
+                }else
+                {
+                    old = tv.getText().toString();
+                    tv.setText(old + "0");
+                }
             }
         });
         Button ison = (Button) findViewById(R.id.ison);
         ison.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View viewrt) {
-                //Random r = new Random();
-                //int i1 = r.nextInt(50) + 1;
-                tv.setText("154465");
+                Random r = new Random();
+                int i1 = r.nextInt(50) + 1;
+                tv.setText(Integer.toString(i1));
                 Toast.makeText(getApplicationContext(),"I told you I was fast, not smart :p", Toast.LENGTH_LONG).show();
             }
         });
@@ -179,7 +325,17 @@ public class MainActivity extends AppCompatActivity {
         synplyn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View viewrt) {
-                countsyn++;
+                aChar = tv.getText().charAt(0);
+                tvend = tv.getText().length();
+                if (aChar != '-' ){
+                        old = tv.getText().toString();
+                        tv.setText("-" + old);
+                }else
+                {
+                    old = tv.getText().subSequence(1,tvend).toString();
+                    tv.setText(old);
+                }
+                /*countsyn++;
                 old = tv.getText().toString();
                 if (countsyn%2==1) {
 
@@ -187,7 +343,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 else{
                     tv.setText(old + "-");
-                }
+                }*/
             }
         });
         Button meion = (Button) findViewById(R.id.meion);
@@ -207,32 +363,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
-
-
-
-
-        // Example of a call to a native method
-        // int[] numbers = {R.id.eight, R.id.nine, R.id.seven, R.id.six, R.id.five, R.id.four, R.id.three, R.id.two, R.id.one, R.id.zero};
+         // int[] numbers = {R.id.eight, R.id.nine, R.id.seven, R.id.six, R.id.five, R.id.four, R.id.three, R.id.two, R.id.one, R.id.zero};
         //better idea: me to pou patietai ena apo ta koumpia not number 8a apo8hkeuetai se int oti periexei to textview
         /// mhden.setOnClickListener((View.OnClickListener) this);
         //  ena.setOnClickListener((View.OnClickListener) this);
 
-        /*clea.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                tv.setText("");
-            }
-        });*/
+
     }
 
-   /* public void ButC(View view) {
-        // Do something in response to button click C
-        TextView tv = (TextView) findViewById(R.id.canvas);
-        tv.setText("000");
-    } */
-
-        /*TODO random number
+         /*TODO random number
         *  int alreadyPickedNumber=3 //Number to exclude
     int number=random.nextInt(9);
     while(number==alreadyPickedNumber){
